552e37fb3c728d3d214ab99c8df1b8d4edd2409e
chingpoi <87354186+chingpoi@users.noreply.github.com>
2022-09-15 10:46:22 +0800

    Initialization

---
+++ Project ART/Controllers/ToolController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_ART.Controllers
+{
+    public class ToolController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
