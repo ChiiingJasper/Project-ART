1de92a977f037cace6c9ccf90fde1e7102a524df
CaptainZala <92006390+CaptainNateo@users.noreply.github.com>
2022-09-27 21:56:56 +0800

    CRUD with TableAssessment

---
+++ Project ART/Controllers/TableAssessmentController.cs
@@ -0,0 +1,156 @@
+﻿using Project_ART.Data;
+using Project_ART.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Project_ART.Controllers
+{
+    public class TableAssessmentController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TableAssessmentController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult TableAssessment()
+        {
+            IEnumerable<TableAssessment> objTableAssessmentList = _db.Assessments;
+            return View(objTableAssessmentList);
+        }
+
+        public IActionResult CreateAssessment()
+        {
+            ViewBag.Exams = GetExams();
+            ViewBag.Interviews = GetInterviews();
+            ViewBag.Users = GetUsers();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateAssessment(TableAssessment obj)
+        {
+            _db.Assessments.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("TableAssessment");
+        }
+
+        public IActionResult UpdateAssessment(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var assessmentFromDb = _db.Assessments.Find(id);
+
+            if (assessmentFromDb == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Exams = GetExams();
+            ViewBag.Interviews = GetInterviews();
+            ViewBag.Users = GetUsers();
+            return View(assessmentFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdateAssessment(TableAssessment obj)
+        {
+            _db.Assessments.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("TableAssessment");
+
+        }
+
+        [HttpGet]
+        public IActionResult DeleteAssessment(int? id)
+        {
+            var assessmentFromDb = _db.Assessments.Find(id);
+            _db.Assessments.Remove(assessmentFromDb);
+            _db.SaveChanges();
+            return RedirectToAction("TableAssessment");
+        }
+
+        //TO GET LISTS
+        private List<SelectListItem> GetExams()
+        {
+            var lstExams = new List<SelectListItem>();
+            foreach (var item in _db.Exams)
+            {
+                lstExams.Add(new SelectListItem()
+                {
+                    Value = item.Exam_ID.ToString(),
+                    Text = item.Exam_Score.ToString()
+                });
+            }
+
+            var defItem = new SelectListItem()
+            {
+                Value = "",
+                Text = "----Select Exam----"
+
+            };
+
+            lstExams.Insert(0, defItem);
+
+            return lstExams;
+        }
+
+        private List<SelectListItem> GetInterviews()
+        {
+            var lstInterview = new List<SelectListItem>();
+            foreach (var item in _db.Interviews)
+            {
+                lstInterview.Add(new SelectListItem()
+                {
+                    Value = item.Interview_ID.ToString(),
+                    Text = item.Interview_Score.ToString()
+                });
+            }
+
+            var defItem = new SelectListItem()
+            {
+                Value = "",
+                Text = "----Select Interview----"
+
+            };
+
+            lstInterview.Insert(0, defItem);
+
+            return lstInterview;
+        }
+
+        private List<SelectListItem> GetUsers()
+        {
+            var lstUsers = new List<SelectListItem>();
+            foreach (var item in _db.Users)
+            {
+                lstUsers.Add(new SelectListItem()
+                {
+                    Value = item.Company_ID.ToString(),
+                    Text = item.First_Name
+                });
+            }
+
+            var defItem = new SelectListItem()
+            {
+                Value = "",
+                Text = "----Select User----"
+
+            };
+
+            lstUsers.Insert(0, defItem);
+
+            return lstUsers;
+        }
+    }
+}
--- Project ART/Models/TableAssessment.cs
+++ Project ART/Models/TableAssessment.cs
@@ -10,20 +10,25 @@ namespace Project_ART.Models
         [DisplayName("Assessment ID")]
         public int Assessment_ID { get; set; }
 
-        [DisplayName("Exam ID")]
-        public int Exam_ID { get; set; }
-
-        [DisplayName("Interview ID")]
-        public int Interview_ID { get; set; }
-
-
-        [DisplayName("Assessed By")]
-        public int? Assessed_By { get; set; }
-
-
-
         [DisplayName("Date Assessed")]
         public DateTime Date_Assessed { get; set; }
-        
+
+        //Foreign Keys
+        [Required]
+        [ForeignKey("Exams")]
+        public int ExamID { get; set; }
+        public virtual TableExam Exams { get; set; }
+
+        [Required]
+        [ForeignKey("Interviews")]
+        public int InterviewID { get; set; }
+        public virtual TableInterview Interviews { get; set; }
+
+        [Required]
+        [ForeignKey("Users")]
+        [DisplayName("Assessed By")]
+        public int CompanyID { get; set; }
+        public virtual TableUser Users { get; set; }
+
     }
 }
