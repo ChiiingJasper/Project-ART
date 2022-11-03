4908299102d7a039b211accd4bd850c82367b6cd
BenzFlores <benamielcastanares@gmail.com>
2022-11-03 15:21:41 +0800

    Final Present Checkpoint meeting Nov 3, 2022

--- Project ART/Controllers/RankingsController.cs
+++ Project ART/Controllers/RankingsController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Project_ART.Data;
+using Project_ART.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Project_ART.Controllers
 {
     public class RankingsController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public RankingsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<TableCandidate> obj = _db.Candidate;
+            return View(obj);
         }
     }
 }
